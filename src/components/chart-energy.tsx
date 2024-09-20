01efdd0a885cc41d127ed77d8ad0c51685275880
Gonzalo Salmeron <gonzxlosalmeron@gmail.com>
2024-09-20 19:20:10 +0200

    feat: Component chart energy

---
+++ src/components/chart-energy.tsx
@@ -0,0 +1,61 @@
+import { useMemo, useRef } from 'react'
+
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+import { ChartOptions } from '@/components/chart-type-picker'
+
+const extractSeriesData = (data: any, key: string) => {
+  return Object.keys(data ?? {}).map((day: string) => ({
+    x: new Date(day).getTime(),
+    y: data[day][key],
+  }))
+}
+
+export default function ChartEnergy({
+  data,
+  value,
+  chartType,
+}: {
+  data: any
+  value: string
+  chartType: string
+}) {
+  const chartOptions = useMemo<Highcharts.Options>(
+    () => ({
+      title: {
+        text: 'Gráfico de Energía',
+      },
+      xAxis: {
+        type: 'datetime',
+      },
+      yAxis: {
+        title: {
+          text: 'Valores',
+        },
+      },
+      series: [
+        {
+          name: value,
+          type: chartType as keyof typeof ChartOptions,
+          data: extractSeriesData(data, value),
+        },
+      ],
+    }),
+    [data, value, chartType]
+  )
+
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null)
+
+  return (
+    <div className='pt-4'>
+      {data && (
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={chartOptions}
+          ref={chartComponentRef}
+        />
+      )}
+    </div>
+  )
+}
