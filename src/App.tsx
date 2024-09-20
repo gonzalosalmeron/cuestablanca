f187d06cea947c6d56b4432b8fa4a901c48a0d52
Gonzalo Salmeron <gonzxlosalmeron@gmail.com>
2024-09-20 18:36:46 +0200

    feat: Dashboard with dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useMemo, useRef, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0);
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+import ChartTypePicker, { ChartOptions } from '@/components/chart-type-picker'
+import DataFrecuence from '@/components/data-frecuence'
+import DatePicker from '@/components/date-picker'
+import ValuePicker from '@/components/value-picker'
+
+import json from '@/assets/data.json'
+
+function extractSeriesData(data: any, key: string) {
+  return Object.keys(data ?? {}).map((day: string) => ({
+    x: new Date(day).getTime(),
+    y: data[day][key],
+  }))
 }
 
-export default App;
+function App() {
+  const [dates, setDates] = useState<Dates>({
+    from: '',
+    to: '',
+  })
+  const [data, setData] = useState([])
+  const [value, setValue] = useState<keyof ApiValue>('Energía Activa')
+  const [chartType, setChartType] = useState('line')
+  const [showChart, setShowChart] = useState(false)
+
+  const chartOptions = useMemo<Highcharts.Options>(
+    () => ({
+      title: {
+        text: 'Gráfico de Energía',
+      },
+      xAxis: {
+        type: 'datetime',
+      },
+      yAxis: {
+        title: {
+          text: 'Valores',
+        },
+      },
+      series: [
+        {
+          name: value,
+          type: chartType as keyof typeof ChartOptions,
+          data: extractSeriesData(data, value),
+        },
+      ],
+    }),
+    [data, value, chartType]
+  )
+
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null)
+
+  // TODO: FETCH API
+
+  return (
+    <div className='p-20'>
+      <div className='flex items-end gap-6'>
+        <DatePicker dates={dates} setDates={setDates} />
+        <DataFrecuence originalData={json} setNewData={setData} />
+        <ValuePicker value={value} setValue={setValue} />
+        <ChartTypePicker chartType={chartType} setChartType={setChartType} />
+        <button
+          onClick={() => setShowChart(true)}
+          className='h-fit bg-green-600 px-4 py-2 text-green-50'
+        >
+          Generar Gráfico
+        </button>
+      </div>
+
+      <div className='pt-4'>
+        {data && showChart && (
+          <HighchartsReact
+            highcharts={Highcharts}
+            options={chartOptions}
+            ref={chartComponentRef}
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
